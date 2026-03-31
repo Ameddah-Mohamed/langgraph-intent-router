9c53cdcc6c4f6d5728a549da59ab8e03ff96e23a
Ameddah Mohamed <mohamed.ameddah@ensia.edu.dz>
2026-03-31 21:30:18 +0100

    feat: add classifier node

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.messages import HumanMessage
 from langgraph.graph.message import add_messages
+from langchain.messages import SystemMessage
 
 load_dotenv()
 
@@ -24,3 +25,16 @@ class State(TypedDict):
 # Structured classifier
 class MessageClassifier(BaseModel):
     message_type: Literal["emotional", "logical"] = Field(...)
+
+
+def classifier_agent(state: State):
+    last_message = state["messages"][-1]
+
+    classifier_llm = llm.with_structured_output(MessageClassifier)
+
+    result = classifier_llm.invoke([
+        SystemMessage("Classify message as emotional or logical"),
+        HumanMessage(last_message.content)
+    ])
+
+    return {"intent": result.message_type}
