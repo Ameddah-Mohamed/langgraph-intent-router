b2cf7235d8aefbcd705d0e2d93d08a3060dc5719
Ameddah Mohamed <mohamed.ameddah@ensia.edu.dz>
2026-03-31 21:29:05 +0100

    feat: add graph state and classifier schema

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Literal
+from pydantic import BaseModel, Field
+
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.messages import HumanMessage
+from langgraph.graph.message import add_messages
 
 load_dotenv()
 
@@ -9,9 +14,13 @@ llm = ChatGoogleGenerativeAI(
     google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
 
-def test_model():
-    response = llm.invoke("Say hello in one sentence")
-    print(response.content)
+# Graph State
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+    intent: str | None
+    next: str | None
+    agent_name: str | None
 
-if __name__ == "__main__":
-    test_model()
+# Structured classifier
+class MessageClassifier(BaseModel):
+    message_type: Literal["emotional", "logical"] = Field(...)
