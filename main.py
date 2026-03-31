17473d9c31d48cf3fd159c79aee0196e6b62b140
Ameddah Mohamed <mohamed.ameddah@ensia.edu.dz>
2026-03-31 21:28:24 +0100

    feat: add Gemini model setup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("LangGraph Intent Router")
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+)
+
+def test_model():
+    response = llm.invoke("Say hello in one sentence")
+    print(response.content)
+
+if __name__ == "__main__":
+    test_model()
